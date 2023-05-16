bd562103b8618c40e5f719741d2d11524fc88dc8
miraziz111 <mirazizazamatov@gmail.com>
2023-05-16 21:40:49 +0500

    ds

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 
 import './App.css';
 import { Footer } from './Components/Footer';
-import Header from './Components/Header/Header';
-import Partners from './Components/Partners/Partners';
+import Header from './Components/Header';
+import Partners from './Components/Partners';
 import Testimonials from './Components/Testimonials/Testimonials';
+import { Work } from './Components/Work/Work';
 function App() {
   return (
 
    <>
     <div>
       <Header />
-   
+     <Work/>
       <Partners />
       <Testimonials />
       <Footer/>
