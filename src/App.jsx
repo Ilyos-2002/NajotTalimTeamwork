4c4ef9b62f2f4a10d14860540aeb20b2a06f9412
Ilyos Xolmamtov <ilyosxolmamatov3@gmail.com>
2023-05-16 21:19:39 +0500

    sdfg

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,15 @@ import Partners from './Components/Partners/Partners';
 import Testimonials from './Components/Testimonials/Testimonials';
 function App() {
   return (
-<<<<<<< HEAD
+
     <div>
       <Header />
       <Partners />
       <Testimonials />
+
+      <Footer />
     </div>
-=======
-   <div>
-    <Header/>
-    <Partners />
-    <Footer/>
-   </div>
->>>>>>> 898f156b8b2e5056c97b82c8a1b32cda0ffcb1d4
+
   )
 }
 export default App;
